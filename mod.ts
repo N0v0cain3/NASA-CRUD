ca5802bafffe1a3a25b41cad44d3d727e6b7f8c4
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-06-30 10:42:06 +0530

    frontend

--- mod.ts
+++ mod.ts
@@ -1,1 +1,7 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+const port = 8000;
+
+app.listen({ port });
+console.log("Listening at 8000");
