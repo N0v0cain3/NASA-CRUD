9d24dfec181fcfafe1421e9217949450a1ce785c
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-06-30 11:01:30 +0530

    static

--- mod.ts
+++ mod.ts
@@ -1,7 +1,20 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 const port = 8000;
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+  await send(ctx, filePath, {
+    root: `${Deno.cwd()}/public`,
+  });
+});
 
 app.listen({ port });
 console.log("Listening at 8000");
